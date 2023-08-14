c93cae3e499e4595b48248efeeef404d768f20b5
Tejau <32521321+Tejau@users.noreply.github.com>
2023-08-14 08:06:08 +0530

    Updated with torrents

--- fasttorrent.mjs
+++ fasttorrent.mjs
@@ -1,5 +1,7 @@
+import info from './package.json';
+assert(info.type === 'json');
 import WebTorrent from 'webtorrent';
-
+import fs from 'fs';
 // Create a client
 const client = new WebTorrent();
 
@@ -7,10 +9,11 @@ const client = new WebTorrent();
 client.maxConns = 500;
 
 // Set download path
-const downloadPath = '/Users/teja/Downloads/personal/breaker';
+const downloadPath = '/Users/teja/Downloads/personal/Untitled';
 
 // Add magnet link to client
-const link = 'magnet:?xt=urn:btih:5EB09E610168CA0CA1D28B1D6994421B12D0F7C9&dn=Kung.Fu.Yoga.2017.HC.HDRip.XviD.AC3-EVO&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce';
+const link = 'magnet:?xt=urn:btih:F6156A11AA8594553A63EDCD5AC337D9A667E402&dn=The%20Fast%20and%20the%20Furious%20(2001)%201080p%20BrRip%20x264%20-%201.40GB%20-%20YIFY&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce'
+
 const torrent = client.add(link, { path: downloadPath });
 
 // Start download
@@ -22,8 +25,16 @@ torrent.on('download', function() {
 
 // Save file
 torrent.on('done', function() {
-  const filePath = `${downloadPath}/${torrent.files[0].path}`;
-  torrent.files[0].moveTo(filePath, function() {
+  const filePath = `${downloadPath}/${torrent.files[0].name}`;
+  const fileStream = torrent.files[0].createReadStream();
+
+  // Create a writable stream to save the file to the desired path
+  const writableStream = fs.createWriteStream(filePath);
+
+  // Pipe the read stream to the write stream to save the file
+  fileStream.pipe(writableStream);
+
+  writableStream.on('finish', function() {
     console.log(`File saved to: ${filePath}`);
   });
 });
